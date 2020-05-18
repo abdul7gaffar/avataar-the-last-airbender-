8fe2713e7dea7755a6f60c5781cd238130e0deee
abdul gaffar <53045088+abdul7gaffar@users.noreply.github.com>
2020-05-19 00:06:03 +0530

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float movespeed;
+    public float jumpspeed;
+    public CharacterController controller;
+    private Vector3 moveDirection;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * movespeed, 0f, Input.GetAxis("Vertical") * movespeed);
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpspeed;
+        }
+        moveDirection.y = moveDirection.y + Physics.gravity.y * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ rotatescript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatescript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float movespeed;
+    public float rotatespeed;
+    public float rotx1;
+    public float roty1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movespeed* 2.5f;
+        }
+        else if (Input.GetKey("w")&& !Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movespeed;
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movespeed;
+        }
+         if (Input.GetKey("s") && !Input.GetKey("d"))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movespeed; 
+        }
+        else if (Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movespeed;
+        }
+
+    }
+    void Update()
+    {
+        rotx1 += Input.GetAxis("Mouse X") * Time.deltaTime * rotatespeed;
+        roty1 -= Input.GetAxis("Mouse Y") * Time.deltaTime * rotatespeed;
+        if (rotx1 < -90)
+        {
+            rotx1 = -90;
+        }
+        else if (rotx1 > 90)
+        {
+            rotx1 = 90;
+        }
+        transform.rotation = Quaternion.Euler(0, roty1, 0);
+        GameObject.FindWithTag("MainCamera").transform.rotation = Quaternion.Euler(rotx1, roty1, 0);
+    }
+}
