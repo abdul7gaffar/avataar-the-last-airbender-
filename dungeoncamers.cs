ad9cf5d112dc5b76d5baeb9ba925a5d554fefcc1
abdul gaffar <53045088+abdul7gaffar@users.noreply.github.com>
2020-05-20 01:44:37 +0530

    Add files via upload

---
+++ dungeoncamers.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dungeoncamers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    public float damping = 1;
+    Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.transform.position + offset;
+        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+        transform.position = position;
+
+        transform.LookAt(target.transform.position);
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ lookAtCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAtCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    void Start()
+    {
+        
+    }
+    void LateUpdate()
+    {
+        transform.LookAt(target.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
